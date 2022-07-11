44961b433ece8fa3e09bd586ecefa7f250a57fa8
Kinshuk Phalke <kp185196@ncr.com>
2022-07-11 10:10:27 -0400

    Minor reformatting

--- pages/api/cart/[...params].js
+++ pages/api/cart/[...params].js
@@ -1,20 +1,25 @@
-import { getCartById, getCartItemsById, deleteCart } from '~/lib/cart';
+import { getCartById, getCartItemsById, deleteCart } from "~/lib/cart";
 
 export default async function handler(req, res) {
-  let siteId = req.query.params[0];
-  let cartId = req.query.params[1];
-  if (req.method == 'DELETE') {
-    const result = await deleteCart(siteId, cartId);
-    res.status(result.status).json({ result, logs: [result.log] });
-  } else {
-    const cart = await getCartById(siteId, cartId);
-    if (cart.status !== 200) {
-      res.status(cart.status).json(cart);
+    console.log("here");
+    let siteId = req.query.params[0];
+    let cartId = req.query.params[1];
+    if (req.method == "DELETE") {
+        const result = await deleteCart(siteId, cartId);
+        res.status(result.status).json({ result, logs: [result.log] });
+    } else {
+        const cart = await getCartById(siteId, cartId);
+        if (cart.status !== 200) {
+            res.status(cart.status).json(cart);
+        }
+        const cartItems = await getCartItemsById(siteId, cartId);
+        if (cartItems.status !== 200) {
+            res.status(cartItems.status).json(cartItems);
+        }
+        res.status(cartItems.status).json({
+            cart,
+            cartItems,
+            logs: [cart.log, cartItems.log],
+        });
     }
-    const cartItems = await getCartItemsById(siteId, cartId);
-    if (cartItems.status !== 200) {
-      res.status(cartItems.status).json(cartItems);
-    }
-    res.status(cartItems.status).json({ cart, cartItems, logs: [cart.log, cartItems.log] });
-  }
 }
